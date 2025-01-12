2bd21606f0cd4bd5c59d6f7196080b46e40df853
CyberCy14 <cyrelljaneradoc14@gmail.com>
2025-01-12 21:55:43 +0800

    changes

--- config/cors.php
+++ config/cors.php
@@ -25,7 +25,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://brewtique-dg3e.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
