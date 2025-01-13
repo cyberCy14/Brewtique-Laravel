1f46d6c1ca6209f2d31eda419863b70e08c6f48b
CyberCy14 <cyrelljaneradoc14@gmail.com>
2025-01-13 21:03:43 +0800

    env updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        config(['api.base_url' => env('http://brewtique-laravel-production.up.railway.app')]);
     }
 
 }
